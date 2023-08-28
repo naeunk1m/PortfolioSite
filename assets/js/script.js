6bc3faa78bfca3414c0354732a9d2124bdb9b483
naeunk1m <08enmik@gmail.com>
2023-08-28 16:17:00 +0900

    20230828

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// gsap.config({ trialWarn: false });
+// console.clear();
+// gsap.registerPlugin(ScrollTrigger, SplitText);
+// const split = new SplitText(".t-ani", { type: "lines" });
+
+// split.lines.forEach((target) => {
+//   gsap.to(target, {
+//     backgroundPositionX: 0,
+//     ease: "none",
+//     scrollTrigger: {
+//       trigger: target,
+//       markers: true,
+//       scrub: 1,
+//       start: "top center",
+//       end: "bottom center"
+//     }
+//   });
+// });
